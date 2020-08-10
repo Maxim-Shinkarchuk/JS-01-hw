90078be138b7fb3202e9ff09feabeaed70df7c54
Maxim-Shinkarchuk <8endlessfire8@gmail.com>
2020-08-10 15:24:04 +0300

    211

---
+++ lesson2-1.js
@@ -0,0 +1,27 @@
+const getItemsString = function (array) {
+  "use strict";
+  // Write code under this line
+  for (const friend of friends) {
+    let string = "";
+  }
+  return string;
+};
+
+console.log(getItemsString(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]));
+/*
+'1 - Mango
+2 - Poly
+3 - Ajax
+4 - Lux
+5 - Jay
+6 - Kong
+'
+*/
+
+//console.log(getItemsString([5, 10, 15]));
+/*
+'1 - 5
+2 - 10
+3 - 15
+'
+*/
