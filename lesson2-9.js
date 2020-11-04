420f8dd2f5e17f1c545c2a68b526e2fc3a4cd352
Maxim-Shinkarchuk <8endlessfire8@gmail.com>
2020-11-04 15:32:07 +0200

    Update lesson2-9.js

--- lesson2-9.js
+++ lesson2-9.js
@@ -6,38 +6,39 @@ function isLoginValid(login, min = 4, max = 16) {
   }
   return isLoginCheck;
 }
-console.log(isLoginValid("hjhjhj"));
+//console.log(isLoginValid("hjhjhj"));
 
 function isLoginUnique(allLogins, login) {
   // Write code under this line
   let checkUniqLogin = true;
-  if (allLogins.includes(allLogins, login)) {
+  if (allLogins.includes(login)) {
     checkUniqLogin = false;
   }
   return checkUniqLogin;
 }
-console.log(isLoginUnique(""));
 
 function addLogin(allLogins, login) {
   "use strict";
   const SUCCESS = "Логин успешно добавлен!";
   const REFUSAL = "Такой логин уже используется!";
   const ERROR = "Ошибка! Логин должен быть размером от 4 до 16 символов";
-  let message;
-  // Write code under this line
-  if ((isLoginValid = false)) {
-    return (message = ERROR);
+  let message = "";
+  if (!isLoginValid(login)) {
+    message = ERROR;
+    return message;
   }
-
-  if ((isLoginUnique = false)) {
+  if (isLoginUnique(allLogins, login)) {
     allLogins.push(login);
-    return (message = SUCCESS);
+    message = SUCCESS;
   } else {
-    return (message = REFUSAL);
+    message = REFUSAL;
   }
+  return message;
 }
 
 const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
+
+console.log(isLoginUnique(logins, "Poly"));
 
 console.log(addLogin(logins, "Ajax"));
 // 'Логин успешно добавлен!'
@@ -50,4 +51,4 @@ console.log(addLogin(logins, "Zod"));
 
 console.log(addLogin(logins, "jqueryisextremelyfast"));
 // 'Ошибка! Логин должен быть от 4 до 16 символов'
-console.log(logins);
+//console.log(logins);
